183c6a6d21ffc7a39926ac386b0875788163ae2e
Chintana Prabhu <chinturp@gmail.com>
2020-03-13 10:14:14 -0700

    Update 191. Number of 1 Bits.java

--- 191. Number of 1 Bits.java
+++ 191. Number of 1 Bits.java
@@ -3,7 +3,10 @@ public class Solution {
     public int hammingWeight(int n) {
         int ones = 0, mask = 1;
         for (int i = 0; i < 32; i++) {
+            if(n == 0)
+                return ones;
             if((n & mask) == mask) {
+                n = n & n-1;           //flip the LSB to 0 at every iteration
                 ones++;
             }
             mask = mask << 1;
